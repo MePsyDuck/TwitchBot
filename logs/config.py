3eb46adcd861abab7d59185e81a771200b4b8300
MePsyDuck <sumit231996@gmail.com>
2022-01-12 21:10:55 +0530

    added more debugging info

--- logs/config.py
+++ logs/config.py
@@ -3,5 +3,6 @@ import os
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 BOT_LOGGER = 'bot'
 LOG_LEVEL = 'DEBUG' if DEBUG else os.environ.get('LOGGING_LEVEL', 'INFO').upper()
-LOG_FORMAT = '%(asctime)s %(levelname)-8s %(funcName)-25s %(message)s'
-LOG_FILENAME = 'bot.log'
+LOG_FORMAT = '%(asctime)s %(levelname)-8s %(name)-25s %(funcName)-25s %(message)s'
+BOT_LOG_FILENAME = 'bot.log'
+ROOT_LOG_FILENAME = 'app.log'
--- logs/logger.py
+++ logs/logger.py
@@ -1,6 +1,6 @@
 import logging
 
-from logs.config import BOT_LOGGER, LOG_FORMAT, LOG_LEVEL, LOG_FILENAME, DEBUG
+from logs.config import BOT_LOGGER, LOG_FORMAT, LOG_LEVEL, BOT_LOG_FILENAME, DEBUG, ROOT_LOG_FILENAME
 
 logger = logging.getLogger(BOT_LOGGER)
 
@@ -9,12 +9,20 @@ def setup_logger():
     log_formatter = logging.Formatter(LOG_FORMAT)
     log_level = logging.getLevelName(LOG_LEVEL)
 
-    # Handlers
-    file_handler = logging.FileHandler(LOG_FILENAME, mode='a')
+    # Root logging
+    file_handler = logging.FileHandler(ROOT_LOG_FILENAME, mode='a')
     file_handler.setFormatter(log_formatter)
-    file_handler.setLevel(log_level)
+    file_handler.setLevel(logging.DEBUG)
 
-    # Internal logging
+    default_logger = logging.getLogger()
+    default_logger.setLevel(log_level)
+    default_logger.addHandler(file_handler)
+
+    # Bot logging
+    file_handler = logging.FileHandler(BOT_LOG_FILENAME, mode='a')
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(logging.DEBUG)
+
     bot_logger = logging.getLogger(BOT_LOGGER)
     bot_logger.setLevel(log_level)
     bot_logger.addHandler(file_handler)
@@ -25,3 +33,4 @@ def setup_logger():
         stream_handler.setLevel(logging.DEBUG)
 
         bot_logger.addHandler(stream_handler)
+        default_logger.addHandler(stream_handler)
