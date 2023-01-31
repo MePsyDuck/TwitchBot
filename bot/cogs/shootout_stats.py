3c50b9fa59ef399f94439f4084972bc4c9a8747b
MePsyDuck <sumit231996@gmail.com>
2023-01-31 10:36:12 +0000

    feat: added shootout logs

--- bot/cogs/shootout_stats.py
+++ bot/cogs/shootout_stats.py
@@ -6,20 +6,20 @@ from twitchio.ext import commands
 
 from bot.cogs.base import BaseCog
 from bot.config import COOLDOWN
-from db import ShootoutStats
+from db import ShootoutLogs, ShootoutStats
 from logs import logger
 
 
 class LastDuel:
-    shooter = None
-    user_shot = None
+    challenger = None
+    target = None
 
-    def update(self, shooter, user_shot):
-        self.shooter = shooter
-        self.user_shot = user_shot
+    def update(self, challenger, target):
+        self.challenger = challenger
+        self.target = target
 
     def get_winner(self, loser):
-        return self.shooter if self.user_shot == loser else self.user_shot
+        return self.challenger if self.target == loser else self.target
 
 
 class ShootoutStatsCog(BaseCog):
@@ -37,51 +37,54 @@ class ShootoutStatsCog(BaseCog):
                 loser = match.group('loser').lower()
                 winner = 'unknown'
 
-                if loser in [self.last_duel.shooter, self.last_duel.user_shot]:
+                if loser in [self.last_duel.challenger, self.last_duel.target]:
                     winner = self.last_duel.get_winner(loser)
                     winner_stats, _ = await ShootoutStats.get_or_create(username=winner)
                     if winner_stats.highest_win_streak == winner_stats.current_win_streak:
                         winner_stats.highest_win_streak = F('highest_win_streak') + 1
                     winner_stats.current_win_streak = F('current_win_streak') + 1
+                    winner_stats.total_won = F('total_won') + 1
                     winner_stats.current_loss_streak = 0
                     await winner_stats.save()
+                else:
+                    logger.critical(f'{loser} not in last duel : {self.last_duel.challenger}, {self.last_duel.target}')
 
                 loser_stats, _ = await ShootoutStats.get_or_create(username=loser)
                 if loser_stats.highest_loss_streak == loser_stats.current_loss_streak:
                     loser_stats.highest_loss_streak = F('highest_loss_streak') + 1
                 loser_stats.current_loss_streak = F('current_loss_streak') + 1
                 loser_stats.current_win_streak = 0
+                loser_stats.total_lost = F('total_lost') + 1
                 await loser_stats.save()
+
+                await ShootoutLogs.create(challenger=self.last_duel.challenger, target=self.last_duel.target, winner=winner, loser=loser)
 
                 logger.info(f'{loser} lost shootout against {winner}')
 
-        elif match := re.search(r'!shootout (?P<user_shot>\S+)( .*)?', message.content):
-            shooter = message.author.name.lower()
+        elif match := re.search(r'!shootout (?P<target>\S+)( .*)?', message.content):
+            challenger = message.author.name.lower()
 
-            shooter_stats, _ = await ShootoutStats.get_or_create(username=shooter)
-            shooter_stats.duels_started = F('duels_started') + 1
-            await shooter_stats.save()
+            challenger_stats, _ = await ShootoutStats.get_or_create(username=challenger)
+            challenger_stats.duels_started = F('duels_started') + 1
+            await challenger_stats.save()
 
-            user_shot = self.get_mentioned_user(match.group('user_shot')) or match.group('user_shot')
-            user_shot = user_shot.lower()
-            user_stats, _ = await ShootoutStats.get_or_create(username=user_shot)
-            user_stats.duels_accepted = F('duels_accepted') + 1
-            await user_stats.save()
+            target = self.get_mentioned_user(match.group('target')) or match.group('target')
+            target = target.lower()
+            target_stats, _ = await ShootoutStats.get_or_create(username=target)
+            target_stats.duels_accepted = F('duels_accepted') + 1
+            await target_stats.save()
 
-            logger.info(f'{shooter} dueled {user_shot}')
+            logger.info(f'{challenger} dueled {target}')
 
-            self.last_duel.update(shooter, user_shot)
+            self.last_duel.update(challenger, target)
 
     @commands.command(aliases=['ss'])
     @commands.cooldown(rate=1, per=COOLDOWN, bucket=commands.Bucket.default)
     async def shootoutstats(self, ctx: commands.Context, *args: str):
-        if ctx.channel.name == 'mepsyduck_':
-            username = self.get_mentioned_user(*args) or ctx.author.name
-            username_lower = username.lower()
+        username = self.get_mentioned_user(*args) or ctx.author.name
+        username_lower = username.lower()
 
-            if stats := await ShootoutStats.get_or_none(username=username_lower):
-                await ctx.send(
-                    f'{username} losses={stats.current_loss_streak}/{stats.highest_loss_streak}, '
-                    f'wins={stats.current_win_streak}/{stats.highest_win_streak},')
-            else:
-                await ctx.send(f'{username} has no stats')
+        if stats := await ShootoutStats.get_or_none(username=username_lower):
+            await ctx.send(f'{stats}')
+        else:
+            await ctx.send(f'{username} has no stats')
--- db/models.py
+++ db/models.py
@@ -47,12 +47,25 @@ class ShootoutStats(Model):
     username = fields.CharField(max_length=64, unique=True)
     current_loss_streak = fields.IntField(default=0)
     highest_loss_streak = fields.IntField(default=0)
+    total_lost = fields.IntField(default=0)
     current_win_streak = fields.IntField(default=0)
     highest_win_streak = fields.IntField(default=0)
+    total_won = fields.IntField(default=0)
     duels_started = fields.IntField(default=0)
     duels_accepted = fields.IntField(default=0)
 
     def __str__(self):
-        s = f'{self.username}, losses={self.current_loss_streak}/{self.highest_loss_streak}, ' \
-            f'wins={self.current_win_streak}/{self.highest_win_streak}, duels_started={self.duels_started}, ' \
-            f'duels_accepted={self.duels_accepted}'
+        return  f'{self.username}({self.duels_started}/{self.duels_accepted}), ' \
+                f'loss :[{self.current_loss_streak}/{self.highest_loss_streak}/{self.total_lost}], ' \
+                f'won :[{self.current_win_streak}/{self.highest_win_streak}/{self.total_won}]'
+
+
+class ShootoutLogs(Model):
+    challenger = fields.CharField(max_length=64)
+    target = fields.CharField(max_length=64)
+    winner = fields.CharField(max_length=64)
+    loser = fields.CharField(max_length=64)
+    when = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self):
+        return  f'{self.challenger} vs {self.target} : {self.loser} lost at {self.when}'
