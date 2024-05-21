6dfe17af0918a94f60755e7c038065260c5850a4
Sumeet Swain <sumit231996@gmail.com>
2024-05-21 22:31:17 +0530

    feat: remove EventSub code

--- bot/cogs/__init__.py
+++ bot/cogs/__init__.py
@@ -3,7 +3,6 @@ from .random_ping_stats import RandomPingStatsCog
 from .shootout_stats import ShootoutStatsCog
 from .admin_cog import AdminCog
 from .chat_log import ChatLoggingCog
-from .channel_info import ChannelInfoCog
 
 
 def prepare(bot):
@@ -12,4 +11,3 @@ def prepare(bot):
     bot.add_cog(ShootoutStatsCog(bot))
     bot.add_cog(AdminCog(bot))
     bot.add_cog(ChatLoggingCog(bot))
-    bot.add_cog(ChannelInfoCog(bot))
--- bot/config.py
+++ bot/config.py
@@ -7,12 +7,8 @@ DEV_NICK = 'mepsyduck_'
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET')
-CALLBACK_URL = os.getenv('CALLBACK_URL')
-LISTEN_PORT = os.getenv('LISTEN_PORT')
 
 CHANNELS = ['singsing', 'mepsyduck_']
 PREFIX = '!!'
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
-
